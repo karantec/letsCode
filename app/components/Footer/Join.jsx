dd11b286d732b3f4d4420e683cef94a34f734dfd
Pradeep Lama Dimdung <tamangjake@gmai.com>
2023-10-03 01:06:48 +0545

    changed fonts and styling on footer

--- app/components/Footer/Join.jsx
+++ app/components/Footer/Join.jsx
@@ -5,9 +5,9 @@ const Join = () => {
             <div className='mx-auto max-w-2xl lg:max-w-7xl sm:py-4 lg:px-8'>
 
                 <div className="text-center">
-                    <h3 className="text-blue text-lg font-normal tracking-widest">JOIN US</h3>
+                    <h3 className="text-white text-xl font-bold tracking-widest">JOIN US</h3>
                     <h2 className="text-4xl sm:text-6xl  text-white font-bold my-6 leading-10"> Take your Journey to <br /> the new level.</h2>
-                    <p className="text-white font-bold text-base ">Educational guidance is primarily concerned with the student's success in their<br/> educational career. We guide students to achieve their dream jobs by providing proper guidance and resources.</p>
+                    <p className="text-white font-semibold text-base ">Educational guidance is primarily concerned with the student's success in their<br/> educational career. We guide students to achieve their dream jobs by providing proper<br /> guidance and resources.</p>
                 </div>
 
                 
